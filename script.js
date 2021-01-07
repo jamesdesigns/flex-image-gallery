f678c341cba3d20b7da7e5fcc7c635988edf22bb
James Hooper <jameshooperdesigns@gmail.com>
2021-01-07 13:53:57 -0700

    Add new images and styles

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function toggleActive(e) {
   console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
+  } else {
+    this.classList.toggle('close');
   }
 }
 
